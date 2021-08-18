23946a6af1ea4b6db16be1418fe3818768a514e8
9007967 <33853639+9007967@users.noreply.github.com>
2021-08-18 16:09:33 +0800

    Update JWT.php
    当$head传入alg值的时候应该覆盖掉第三个$alg参数的默认值
    兼容PHP8的写法
    JWT::encode(payload: $payload, key: self::$privateKey, head: self::$head);

--- src/JWT.php
+++ src/JWT.php
@@ -176,7 +176,7 @@ class JWT
             $header['kid'] = $keyId;
         }
         if (isset($head) && \is_array($head)) {
-            $header = \array_merge($head, $header);
+            $header = \array_merge($header, $head);
         }
         $segments = array();
         $segments[] = static::urlsafeB64Encode(static::jsonEncode($header));
